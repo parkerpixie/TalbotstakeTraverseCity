8cf3bba5f72695eecbab9edef2272357755bc398
parkerpixie <parkerpixiedust@gmail.com>
2026-07-30 22:00:04 -0500

    Refresh Adventure dashboard and family heart assets

--- navigation-ux.js
+++ navigation-ux.js
@@ -15,11 +15,12 @@
 
   loadStyle('discover-field-guide.css?v=20260730-2');
   loadStyle('choose-explorer.css?v=20260730-2');
-  loadStyle('your-adventure.css?v=20260730-4');
+  loadStyle('your-adventure.css?v=20260730-5');
   loadStyle('places-hub.css?v=20260730-2');
+  loadStyle('favorite-attribution.css?v=20260730-2');
   loadScript('discover-field-guide.js?v=20260730-2');
   loadScript('choose-explorer.js?v=20260730-2');
-  loadScript('your-adventure.js?v=20260730-4');
+  loadScript('your-adventure.js?v=20260730-5');
   loadScript('places-hub.js?v=20260730-2');
 
   const appShell = document.getElementById('appShell');
