f4c29bf410d95dedf7ce18c4c241950a329b0069
parkerpixie <parkerpixiedust@gmail.com>
2026-07-30 20:37:52 -0500

    Load personalized Your Adventure dashboard

--- navigation-ux.js
+++ navigation-ux.js
@@ -14,9 +14,11 @@
   };
 
   loadStyle('discover-field-guide.css?v=20260730-2');
-  loadStyle('choose-explorer.css?v=20260730-1');
+  loadStyle('choose-explorer.css?v=20260730-2');
+  loadStyle('your-adventure.css?v=20260730-1');
   loadScript('discover-field-guide.js?v=20260730-2');
-  loadScript('choose-explorer.js?v=20260730-1');
+  loadScript('choose-explorer.js?v=20260730-2');
+  loadScript('your-adventure.js?v=20260730-1');
 
   const appShell = document.getElementById('appShell');
   const appHeader = document.querySelector('.app-header');
