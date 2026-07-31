93ffcc1e69f18f6b16bb4376e4b0a0ac9995e5ac
parkerpixie <parkerpixiedust@gmail.com>
2026-07-30 21:34:24 -0500

    Bust Places hub script cache

--- navigation-ux.js
+++ navigation-ux.js
@@ -20,7 +20,7 @@
   loadScript('discover-field-guide.js?v=20260730-2');
   loadScript('choose-explorer.js?v=20260730-2');
   loadScript('your-adventure.js?v=20260730-3');
-  loadScript('places-hub.js?v=20260730-1');
+  loadScript('places-hub.js?v=20260730-2');
 
   const appShell = document.getElementById('appShell');
   const appHeader = document.querySelector('.app-header');
