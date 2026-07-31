731514fb06586ab8dd0faa70e1226f6a6d4197e5
parkerpixie <parkerpixiedust@gmail.com>
2026-07-30 20:28:50 -0500

    Refresh cache for explorer-first landing

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='talbots-tc-v4';
+const CACHE='talbots-tc-v5';
 const CORE=['/','/index.html','/styles.css','/planner-upgrades.css','/app.js','/presentation-polish.js','/gallery-correction.js','/debut-upgrades.js','/debut-upgrades.css','/house-final-upgrades.js','/room-selection-gallery.css','/navigation-ux.js','/navigation-ux.css','/discover-field-guide.js','/discover-field-guide.css','/choose-explorer.js','/choose-explorer.css','/manifest.webmanifest','/app-icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
